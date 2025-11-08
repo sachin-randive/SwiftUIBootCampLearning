7a9b8776a50a1df4ec5a5dc8d4f3cc1e5d701ce6
Sachin Randive <r.sachin81@gmail.com>
2025-11-08 16:43:56 +0530

    Implemented Button Functionality.

--- Button/ButtonModule.swift
+++ Button/ButtonModule.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ButtonModule: View {
+    @State private var rectanleColor = Color.blue
+    @State var title = "Hello"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Rectangle()
+            //.fill(Color.blue)
+                .frame(width: 200, height: 100)
+                .cornerRadius(25)
+                .foregroundColor(rectanleColor)
+            Text(title)
+                .font(.headline)
+            
+            Button("Click Me") {
+                rectanleColor = .red
+                title = "Button Clicked"
+                
+            }
+            .padding(10)
+            
+            Button{
+                rectanleColor = .yellow
+                title = "custom Label Clicked"
+                
+            }label: {
+                Text("Custom Label")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .frame(width: 360, height: 48)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            
+            Button {
+                rectanleColor = .purple
+                title = "heart circle clicked"
+                
+            } label: {
+                Image(systemName: "heart.circle.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 
