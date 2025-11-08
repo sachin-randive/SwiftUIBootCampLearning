df2cf43f4a0564463c48076f8a8ffcf68144cdcf
Sachin Randive <r.sachin81@gmail.com>
2025-11-08 16:09:28 +0530

    Implemented ButtonModule functionality

---
+++ Button/ButtonModule.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonModule.swift
+//  SwiftUILearning
+//
+//  Created by Sachin Randive on 08/11/25.
+//
+
+import SwiftUI
+
+struct ButtonModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonModule()
+}
--- List/ListModules.swift
+++ List/ListModules.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ListModules: View {
-    let drivers: [String] = ["Ravi Patel","Divit Randive", "Anu sharma", "Depak Pandy"]
+    @State var drivers: [String] = ["Ravi Patel","Divit Randive", "Anu sharma", "Depak Pandy"]
     
-    let teams: [String] = ["Mumbai Indians","delhi capitals", "Chennai Super Kings", ]
+    @State var teams: [String] = ["Mumbai Indians","delhi capitals", "Chennai Super Kings", ]
     var body: some View {
         List {
             Section(header: Text("Drivers")) {
                 ForEach(drivers, id: \.self) { driver in
                     Text(driver)
                 }
+                .onDelete(perform: { offsets in
+                    drivers.remove(atOffsets: offsets)
+                })
             }
             
             Section(header: Text("Teams")) {
                 ForEach(teams, id: \.self) { team in
                     Text(team)
                 }
+                .onDelete(perform: { offset in
+                    teams.remove(atOffsets: offset)
+                })
             }
             
         }.listStyle(SidebarListStyle())
