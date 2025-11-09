3375152687a85ae60b561a76730b8f3ce2fb0832
Sachin Randive <r.sachin81@gmail.com>
2025-11-09 17:28:14 +0530

    Added Page and pagination on Tabview

---
+++ TabView/TabViewModule.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewModule.swift
+//  SwiftUILearning
+//
+//  Created by Sachin Randive on 09/11/25.
+//
+
+import SwiftUI
+
+struct TabViewModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewModule()
+}
