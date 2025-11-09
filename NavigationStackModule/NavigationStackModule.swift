a1426900f930a80c3a1d4b3e6e65b3357d6ad358
Sachin Randive <r.sachin81@gmail.com>
2025-11-09 16:18:13 +0530

    Added sheets and Navigation views

---
+++ NavigationStackModule/NavigationStackModule.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackModule.swift
+//  SwiftUILearning
+//
+//  Created by Sachin Randive on 09/11/25.
+//
+
+import SwiftUI
+
+struct NavigationStackModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackModule()
+}
---
+++ Sheets/SheetModule.swift
@@ -0,0 +1,18 @@
+//
+//  SheetModule.swift
+//  SwiftUILearning
+//
+//  Created by Sachin Randive on 08/11/25.
+//
+
+import SwiftUI
+
+struct SheetModule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetModule()
+}
