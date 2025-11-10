8d8f4ec3a4a0772ad8cc5b4503a2fc79e15c2032
Sachin Randive <r.sachin81@gmail.com>
2025-11-10 10:08:18 +0530

    Added View And ViewModel class for MVVM

--- Grid/GridModules.swift
+++ Grid/GridModules.swift
@@ -6,23 +6,33 @@
 //
 
 import SwiftUI
-
+// Model
 struct Driver {
     var driverPic: String
     var driverId: Int
 }
-
+// ViewModel
+class DriverViewModel: ObservableObject {
+    @Published var drivers: [Driver] = []
+    init () {
+        fetchDrivers()
+    }
+    func fetchDrivers() {
+        self.drivers = [Driver(driverPic:"ab", driverId: 1),Driver(driverPic:"ac", driverId: 2),Driver(driverPic:"ad", driverId: 3),Driver(driverPic:"ae", driverId: 4),Driver(driverPic:"Img", driverId: 5),Driver(driverPic:"ac", driverId: 6),Driver(driverPic:"ad", driverId: 7),Driver(driverPic:"ae", driverId: 8),Driver(driverPic:"Img", driverId: 9,)]
+    }
+}
+// View
 struct GridModules: View {
     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     let dimention = (UIScreen.main.bounds.width/3)
-    var driverImages  = [Driver(driverPic:"ab", driverId: 1),Driver(driverPic:"ac", driverId: 2),Driver(driverPic:"ad", driverId: 3),Driver(driverPic:"ae", driverId: 4),Driver(driverPic:"Img", driverId: 5),Driver(driverPic:"ac", driverId: 6),Driver(driverPic:"ad", driverId: 7),Driver(driverPic:"ae", driverId: 8),Driver(driverPic:"Img", driverId: 9,)]
-    //["ab","ac","ad","ae","Img","ac","ad","ae","Img"]
+    
+    @StateObject private var viewModel = DriverViewModel()
     
     var body: some View {
        ScrollView {
            LazyVGrid(columns: columns, spacing: 2) {
               
-               ForEach(driverImages,  id: \.driverId) { item in
+               ForEach(viewModel.drivers,  id: \.driverId) { item in
 //                   Text("Module \(item.driverPic)")
 //                       .font(.headline)
 //                       .padding()
--- ViewExtractModelData/ViewExactModule.swift
+++ ViewExtractModelData/ViewExactModule.swift
@@ -6,20 +6,29 @@
 //
 
 import SwiftUI
-
+//ViewModel
+class ViewExactViewModel: ObservableObject {
+    @Published var foodItems:[FoodItemModel] = []
+    
+    init () {
+        fetchFoodItems()
+    }
+    func fetchFoodItems() {
+        self.foodItems = [.init(imageName: "1", type: "Japanies", restourantName: "shoto"), .init(imageName: "2", type: "American", restourantName: "Mohu"), .init(imageName: "1", type: "Indian", restourantName: "Taj"), .init(imageName: "3", type: "Jarmanian", restourantName: "SHahaji"), .init(imageName: "4", type: "Japanies", restourantName: "New Place"), .init(imageName: "5", type: "Indian", restourantName: "Rajweada"), .init(imageName: "1", type: "Japanies", restourantName: "shoto"), .init(imageName: "2", type: "American", restourantName: "Mohu"), .init(imageName: "1", type: "Indian", restourantName: "Taj"), .init(imageName: "3", type: "Jarmanian", restourantName: "SHahaji"), .init(imageName: "4", type: "Japanies", restourantName: "New Place"), .init(imageName: "5", type: "Indian", restourantName: "Rajweada")]
+    }
+}
+// View
 struct ViewExactModule: View {
-   // let foodImages = ["2","1","3","4","5","1","2","3","4", "5"]
-    let foodItems:[FoodItemModel] = [.init(imageName: "1", type: "Japanies", restourantName: "shoto"), .init(imageName: "2", type: "American", restourantName: "Mohu"), .init(imageName: "1", type: "Indian", restourantName: "Taj"), .init(imageName: "3", type: "Jarmanian", restourantName: "SHahaji"), .init(imageName: "4", type: "Japanies", restourantName: "New Place"), .init(imageName: "5", type: "Indian", restourantName: "Rajweada"), .init(imageName: "1", type: "Japanies", restourantName: "shoto"), .init(imageName: "2", type: "American", restourantName: "Mohu"), .init(imageName: "1", type: "Indian", restourantName: "Taj"), .init(imageName: "3", type: "Jarmanian", restourantName: "SHahaji"), .init(imageName: "4", type: "Japanies", restourantName: "New Place"), .init(imageName: "5", type: "Indian", restourantName: "Rajweada")]
+    @StateObject private var viewModel:ViewExactViewModel = .init()
     
     var body: some View {
-        
         Text("Featured on Swift Food")
             .font(.title2)
             .fontWeight(.semibold)
         
         ScrollView {
             VStack(spacing: 40) {
-                ForEach(foodItems) { foodItem in
+                ForEach(viewModel.foodItems) { foodItem in
                     MenuItemView(foodItemMenu: foodItem)
                 }
             }
